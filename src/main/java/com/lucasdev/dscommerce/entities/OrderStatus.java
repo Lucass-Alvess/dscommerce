fa7b9f38b41f191c5f84823f4d61fc3601867dc4
Lucass-Alvess <lucas_oliveira-01@outlook.com>
2025-08-04 17:56:01 -0300

    first commit

---
+++ src/main/java/com/lucasdev/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,6 @@
+package com.lucasdev.dscommerce.entities;
+
+public enum OrderStatus {
+
+    WATTING_PATMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+}
---
+++ src/main/java/com/lucasdev/dscommerce/entities/Payment.java
@@ -0,0 +1,60 @@
+package com.lucasdev.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Payment payment = (Payment) o;
+        return Objects.equals(id, payment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
