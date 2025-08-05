6f2b560d208766eaef71d83192ce7ef2f8707a1a
Lucass-Alvess <lucas_oliveira-01@outlook.com>
2025-08-05 20:01:14 -0300

    Post new product

--- src/main/java/com/lucasdev/dscommerce/services/ProductService.java
+++ src/main/java/com/lucasdev/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.lucasdev.dscommerce.dto.ProductDTO;
 import com.lucasdev.dscommerce.entities.Product;
 import com.lucasdev.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -22,4 +25,23 @@ public class ProductService {
         ProductDTO dto = new ProductDTO(product);
         return dto;
     }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
+    }
+
+    @Transactional
+    public ProductDTO insert(ProductDTO dto) {
+        Product entity = new Product();
+        entity.setName(dto.getName());
+        entity.setDescription(dto.getDescription());
+        entity.setPrice(dto.getPrice());
+        entity.setImgUrl(dto.getImgUrl());
+
+        entity = repository.save(entity);
+        return new ProductDTO(entity);
+    }
+
 }
