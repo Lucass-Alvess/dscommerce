401938a880d57321565e8b11dcda3ddd9a210285
Lucass-Alvess <lucas_oliveira-01@outlook.com>
2025-08-11 19:12:18 -0300

    Bean Validation

---
+++ src/main/java/com/lucasdev/dscommerce/dto/ValidationError.java
@@ -0,0 +1,23 @@
+package com.lucasdev.dscommerce.dto;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends CustomError {
+
+    private List<FieldMessage> erros = new ArrayList<>();
+
+    public ValidationError(Instant timesTamp, Integer status, String error, String path) {
+        super(timesTamp, status, error, path);
+    }
+
+    public List<FieldMessage> getErros() {
+        return erros;
+    }
+
+    public void addError(String fieldName, String message) {
+        erros.add(new FieldMessage(fieldName, message));
+    }
+
+}
