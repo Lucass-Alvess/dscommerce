19532eb845f54b9eb4a79a7c0b816eb5513c00d5
Lucass-Alvess <lucas_oliveira-01@outlook.com>
2025-10-08 19:57:34 -0300

    OAuth2, JWT, password grant

--- src/main/java/com/lucasdev/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/lucasdev/dscommerce/dto/ProductDTO.java
@@ -2,7 +2,6 @@ package com.lucasdev.dscommerce.dto;
 
 import com.lucasdev.dscommerce.entities.Product;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
--- src/main/java/com/lucasdev/dscommerce/repositories/UserRepository.java
+++ src/main/java/com/lucasdev/dscommerce/repositories/UserRepository.java
@@ -1,18 +1,21 @@
 package com.lucasdev.dscommerce.repositories;
 
+import com.lucasdev.dscommerce.entities.User;
 import com.lucasdev.dscommerce.projection.UserDetailsProjection;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query(nativeQuery = true, value = """
-			SELECT tb_user.email AS username, tb_user.password, tb_role.id AS roleId, tb_role.authority
-			FROM tb_user
-			INNER JOIN tb_user_role ON tb_user.id = tb_user_role.user_id
-			INNER JOIN tb_role ON tb_role.id = tb_user_role.role_id
-			WHERE tb_user.email = :email
-		""")
-    List<UserDetailsProjection> searchUserAndRolesByEmail(String email);
+	@Query(nativeQuery = true, value = """
+            SELECT tb_user.email AS username, tb_user.password, tb_role.id AS roleId, tb_role.authority
+            FROM tb_user
+            INNER JOIN tb_user_role ON tb_user.id = tb_user_role.user_id
+            INNER JOIN tb_role ON tb_role.id = tb_user_role.role_id
+            WHERE tb_user.email = :email
+        """)
+	List<UserDetailsProjection> searchUserAndRolesByEmail(@Param("email") String email);
 }
